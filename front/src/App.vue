b59fa6a8bb3342a6faaf5b953b86ef03d461db83
Bruno Moraes <brunodelciomoraes@gmail.com>
2020-09-19 14:17:44 -0300

    Ajustes pagina login

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,29 +1,21 @@
 <template>
 <body>
-  <img
-    src="./assets/img/LOGO.png"
-    style="display:block; margin-left: auto; margin-right:auto; margin-top: 20px; width: 600px;"
-  />
-  <div class="card" style="width: 500px; margin: auto;padding: 25px;margin-top: 80px;">
+  <img class="logo" src="./assets/img/LOGO.png" />
+  <div class="card">
     <form method="POST">
       <div class="form-group">
         <label for="exampleInputEmail1">Email</label>
-        <input
-          type="email"
-          class="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-        />
+        <input type="email" class="form-control" id="InputEmail" aria-describedby="emailHelp" />
       </div>
       <div class="form-group">
         <label for="exampleInputPassword1">Senha</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" />
+        <input type="password" class="form-control" id="InputPassword" />
       </div>
       <div class="form-group form-check">
         <input type="checkbox" class="form-check-input" id="exampleCheck1" />
         <label class="form-check-label" for="exampleCheck1">Check me out</label>
       </div>
-      <button type="submit" class="btn btn-primary" style="background-color: orange;">Login</button>
+      <button type="submit" class="btn btn-primary btsubtmit">Login</button>
     </form>
   </div>
 </body>
@@ -35,6 +27,25 @@ export default {};
 
 <style>
 body {
-  background-image: url('./assets/img/bg.jpg');
+  background-image: url("./assets/img/bg.jpg");
+}
+
+.card {
+  width: 500px;
+  margin: auto;
+  padding: 25px;
+  margin-top: 40px;
+}
+
+.logo {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 20px;
+  width: 600px;
+}
+
+.btsubmit {
+  background-color: orange;
 }
 </style>
