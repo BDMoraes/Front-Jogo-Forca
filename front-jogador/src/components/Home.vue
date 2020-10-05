6c4a20daa46f86c7c31c3c8c92ff472c9f0c53d3
Bruno Moraes <brunodelciomoraes@gmail.com>
2020-10-05 20:31:59 -0300

    Estrutura jogo

---
+++ front-jogador/src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home"></div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<style>
+.home {
+  background-color: white;
+  width: 60%;
+  height: 70%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
