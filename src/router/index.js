5aa772968a1b3aed953062f67d418e24a434229d
Stephano Ramos <stephano.ramos@thoughtworks.com>
2020-10-15 00:51:32 -0300

    Utiliza somente um projeto

---
+++ src/router/index.js
@@ -0,0 +1,61 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Dashboard from '@/views/Dashboard';
+import Login from '@/views/Login';
+import Home from '@/views/Home';
+import store from '@/config/store';
+import Categoria from '@/views/Categoria';
+import Jogo from '@/views/Jogo';
+import Final from '@/views/Final';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    name: 'Home',
+    path: '',
+    component: Home,
+  },
+  {
+    name: 'Categoria',
+    path: '/categoria',
+    component: Categoria,
+  },
+  {
+    name: 'Jogo',
+    path: '/jogo/:categoria',
+    component: Jogo,
+  },
+  {
+    name: 'Final',
+    path: '/final/:pontuacao',
+    component: Final,
+  },
+  {
+    name: 'Login',
+    path: '/login',
+    component: Login,
+  },
+  {
+    name: 'Dashboard',
+    path: '/dashboard',
+    component: Dashboard,
+    meta: {
+      guarded: true,
+    },
+  }];
+
+const router = new VueRouter({
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  let redirect = to.name !== 'Login' &&
+      to.meta.guarded &&
+      store.state.accessToken === null;
+
+  redirect ? next({name: 'Login'}) : next();
+});
+
+export default router;
+
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="admin-pages">
+    <PageTitle
+      icon="fa fa-cogs"
+      main="Administração do Sistema"
+      sub="Cadastros"
+    />
+    <div class="admin-pages-tabs">
+      <b-card no-body>
+        <b-tabs card>
+          <b-tab title="Categorias" active>
+            <AdminCategorias />
+          </b-tab>
+          <b-tab title="Palavras">
+            <AdminPalavras />
+          </b-tab>
+          <b-tab title="Questões">
+            <AdminQuestoes />
+          </b-tab>
+        </b-tabs>
+      </b-card>
+    </div>
+  </div>
+</template>
+
+<script>
+import PageTitle from '../components/PageTitle';
+import AdminCategorias from '../components/AdminCategorias';
+import AdminPalavras from '../components/AdminPalavras';
+import AdminQuestoes from '../components/AdminQuestoes';
+
+export default {
+  name: 'Dashboard',
+  components: {PageTitle, AdminCategorias, AdminPalavras, AdminQuestoes},
+};
+</script>
+
+<style>
+</style>
